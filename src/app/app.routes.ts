c34333656e9f1501d71afac822eae5b4ad5ddbf1
mo0olto <mostafa.allam.de@gmail.com>
2026-08-06 19:27:41 +0300

    project init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        loadComponent: () =>
+          import('../app/pages/home/home').then((m) => m.Home),
+        title: 'Digital Bond | Digital Marketing Agency',
+      },
+      {
+        path: 'done',
+        loadComponent: () =>
+          import('../app/pages/done/done').then((m) => m.Done),
+        title: 'Thank You | Digital Bond',
+      },
+      {
+        path: '**',
+        redirectTo: '',
+      },
+
+];
